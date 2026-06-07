15fe335252752f17dd7562eb76a33a60f229155d
GlebPishchalo <56067349+GlebPishchalo@users.noreply.github.com>
2026-06-07 19:47:33 +0300

    Create: Lesson_1

--- src/MyFirstTask.java
+++
@@ -1,7 +0,0 @@
-public class MyFirstTask {
-    public static void main(String[] args) {
-        System.out.println("Мое первое задание на Java 11!");
-        int sum = 5 + 3;
-        System.out.println("Результат: " + sum);
-    }
-}
