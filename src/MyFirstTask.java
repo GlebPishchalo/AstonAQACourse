4130991ecff87dc4ccd83d9214c40e55d08be282
GlebPishchalo <56067349+GlebPishchalo@users.noreply.github.com>
2026-06-07 19:44:07 +0300

    Мой первый коммит

---
+++ src/MyFirstTask.java
@@ -0,0 +1,7 @@
+public class MyFirstTask {
+    public static void main(String[] args) {
+        System.out.println("Мое первое задание на Java 11!");
+        int sum = 5 + 3;
+        System.out.println("Результат: " + sum);
+    }
+}
